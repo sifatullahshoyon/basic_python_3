a443aa0a8f5301ef469552d920720800f949eb50
Sifat Ullah Shoyon <sifatullah.shoyon@gmail.com>
2026-06-10 19:02:23 +0600

    built in modules

---
+++ build_in_modules.py
@@ -0,0 +1,12 @@
+from math import *
+from random import *
+from time import sleep
+
+result_ceil = ceil(4.000641)
+result_floor = floor(4.999999)
+# print(result_floor)
+
+print(random())
+print(randint(1,100))
+sleep(3)
+print(choice(['safin','shuvo', 'shohan', 'shoyon', 'mridul', 'billa', 'alamin', 'nazmul']))
